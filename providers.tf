9f2ac1e7c7e9cf90aff254ad6feab7dd34248b07
Thirukonda, Prasanna <prasanna.thirukonda@wirecard.com>
2020-07-27 11:52:34 +0400

    Modified the secret keys

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA5ARD3TRGWZEZV3XJ"
-  secret_key = "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"
+  access_key = "AKIAXF4KKI4YBIG2ENF5"
+  secret_key = "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"
 }
